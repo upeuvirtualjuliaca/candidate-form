39a86856b4fff69fdf1545674cd3deb157d4463e
Vidal Mamani Jordan <vidal_mamani@upeu.edu.pe>
2026-05-04 16:16:00 -0500

    Updates

---
+++ assets/usePermissions-BdMeAdrQ.js
@@ -0,0 +1,1 @@
+import{o as e}from"./runtime-core.esm-bundler-DdhvUMNO.js";import{t}from"./index-l6nvyI_9.js";function n(){let{currentRole:n}=t();return{canWrite:e(()=>n.value===`admin`||n.value===`recruiter`),canAdmin:e(()=>n.value===`admin`),isViewer:e(()=>n.value===`viewer`)}}export{n as t};
