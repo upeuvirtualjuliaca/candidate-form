b78f47b45206f780d44a776809d2adff7f8fa96a
upeuvirtualjuliaca <upeuvirtualjuliaca@users.noreply.github.com>
2026-05-12 14:59:05 +0000

    deploy: 2115c800ae31fe061a87683abd482d492dec3250

--- assets/usePermissions-DhbTwGV_.js
+++
@@ -1,1 +0,0 @@
-import{o as e}from"./runtime-core.esm-bundler-DdhvUMNO.js";import{t}from"./index-CTDH-zsn.js";function n(){let{currentRole:n}=t();return{canWrite:e(()=>n.value===`admin`||n.value===`recruiter`),canAdmin:e(()=>n.value===`admin`),isViewer:e(()=>n.value===`viewer`)}}export{n as t};
